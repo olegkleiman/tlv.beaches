5f3a08e6886284cb9bd6062ba8ba2ca466ff3dee
Oleg Kleiman <oleg_kleyman@hotmail.com>
2022-07-10 17:17:18 +0300

    Initial Commit

---
+++ tlv.beaches/tlv_beachesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tlv_beachesApp.swift
+//  tlv.beaches
+//
+//  Created by Oleg Kleiman on 10/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct tlv_beachesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
